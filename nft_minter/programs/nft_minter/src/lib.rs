bd524d19e18157c5a2f1522107b6d3dc5f90115c
Andre DC Lanzoni <andre.dc.lanzoni@pm.me>
2025-05-21 12:50:36 +0100

    feat: add initialize_collection instruction

---
+++ nft_minter/programs/nft_minter/src/lib.rs
@@ -0,0 +1,36 @@
+use anchor_lang::prelude::*;
+
+declare_id!("ReplaceWithYourProgramID11111111111111111111111111");
+
+#[program]
+pub mod nft_minter {
+    use super::*;
+
+    pub fn initialize_collection(
+        ctx: Context<InitializeCollection>,
+        name: String,
+        symbol: String,
+    ) -> Result<()> {
+        let collection = &mut ctx.accounts.collection;
+        collection.authority = *ctx.accounts.authority.key;
+        collection.name = name;
+        collection.symbol = symbol;
+        Ok(())
+    }
+}
+
+#[account]
+pub struct Collection {
+    pub authority: Pubkey,
+    pub name: String,
+    pub symbol: String,
+}
+
+#[derive(Accounts)]
+pub struct InitializeCollection<'info> {
+    #[account(init, payer = authority, space = 8 + 32 + (4 + 32) + (4 + 10))]
+    pub collection: Account<'info, Collection>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
